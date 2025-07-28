55fba405e29de1a49f0b2af84b9d24a7cff98592
brianpeters2 <104334150+brianpeters2@users.noreply.github.com>
2025-07-27 23:51:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+fetch('https://fedskillstest.coalitiontechnologies.workers.dev')
+  .then(res => res.json())
+  .then(data => {
+    const jessica = data.find(p => p.name === "Jessica Taylor");
+    if (jessica) {
+      displayPatientInfo(jessica);
+      displayChart(jessica.diagnosis_history);
+    }
+  });
 
